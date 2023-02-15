b83d93c76e42a858843d3be6b7d5ad0b066771ca
Ben Jaques-Lelsie <ben.jaques-leslie@gsa.gov>
2023-02-15 10:09:37 -0600

    Updating function

--- R/nice_date.R
+++ R/nice_date.R
@@ -1,24 +1,21 @@
-
 #' Returns a nice looking date for text
 #'
 #' @param in_date Any date value
 #' @param abbr Boolean whether or not to abbreviate month name
 #'
-#' @return Text string of the date
+#' @return Text sting of date
 #' @export
 #' @import lubridate
 #' @import tidyverse
 #' @import glue
 #'
-#' @examples Enter as_date('2021-01-01'), returns "January 1, 2021"
-#' @examples Enter as_date('2021-01-01',abbr = TRUE), returns "Jan 1, 2021"
+#' @examples Enter nice_date('2021-01-01'), returns "January 1, 2021"
+#' @examples Enter nice_date('2021-01-01',abbr = TRUE), returns "Jan 1, 2021"
 nice_date <- function(in_date, abbr = FALSE)
 {
   m_ <- in_date %>% month(label = TRUE, abbr = abbr)
   d_ <- in_date %>% day()
   y_ <- in_date %>% year()
-
   out <- glue::glue("{m_} {d_}, {y_}")
-
   return(out)
 }
