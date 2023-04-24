a4ad5f404ca57827ed46f31cde75849e9c56faa4
Ben Jaques-Lelsie <ben.jaques-leslie@gsa.gov>
2023-04-24 12:25:26 -0500

    updates from typos

--- R/getmode.R
+++ R/getmode.R
@@ -1,15 +1,15 @@
 #' Calculate the mode of a vector
 #'
-#' @param v The vector overwhich to calcualt the mode.
+#' @param v The vector over which to calculates the mode.
 #'
 #' @return The mode of the vector.
 #' @export
 #'
 #' @examples
 getmode <- function(v) {
-  
+
   uniqv <- unique(v)
-  
+
   uniqv[which.max(tabulate(match(v, uniqv)))]
-  
+
 }
